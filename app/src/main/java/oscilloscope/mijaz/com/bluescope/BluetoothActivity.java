a42a47224d7b4cc98017c9e78110c6892ad58d2e
mijazm <mijazm1@gmail.com>
2016-02-11 20:26:34 +0530

    Initial Commit

---
+++ app/src/main/java/oscilloscope/mijaz/com/bluescope/BluetoothActivity.java
@@ -0,0 +1,360 @@
+package oscilloscope.mijaz.com.bluescope;
+
+/**
+ * Created by eee on 3/5/2015.
+ */
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.PowerManager;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
+
+public class BluetoothActivity extends Activity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener {
+
+    String prescaler_string = "p128",threshold_string = "t50";
+
+    int seekbar_value;
+
+    BluetoothAdapter mBluetoothAdapter;
+    BluetoothSocket mmSocket;
+    BluetoothDevice mmDevice;
+    OutputStream mmOutputStream;
+    InputStream mmInputStream;
+    Thread workerThread;
+    public int[] readBuffer;
+    int readBufferPosition;
+    int counter;
+    volatile boolean stopWorker;
+    protected PowerManager.WakeLock mWakeLock;
+
+    private TextView prescaler_value,threshold_value,frequency_value;
+    private Button mConnectButton;
+    private Button run_buton,pause_button;
+    private SeekBar seekbar_p,seekbar_t,seekbar_f;
+
+    // Intent request codes
+    private static final int REQUEST_CONNECT_DEVICE = 1;
+    private static final int REQUEST_ENABLE_BT = 2;
+
+    private boolean bready = false;
+    private boolean pause = false;
+
+    public WaveformView mWaveform = null;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bluetooth);
+
+        prescaler_value = (TextView) findViewById(R.id.prescaler);
+        threshold_value = (TextView) findViewById(R.id.threshold);
+        frequency_value = (TextView) findViewById(R.id.frequency);
+
+        seekbar_f = (SeekBar) findViewById(R.id.seekBar_frequency);
+        seekbar_t = (SeekBar) findViewById(R.id.seekBar_threshold);
+        seekbar_p = (SeekBar) findViewById(R.id.seekBar_prescaler);
+
+        seekbar_f.setOnSeekBarChangeListener(this);
+        seekbar_t.setOnSeekBarChangeListener(this);
+        seekbar_p.setOnSeekBarChangeListener(this);
+
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        this.mWakeLock = pm.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK, "My Tag");
+        this.mWakeLock.acquire();
+
+
+        run_buton = (Button) findViewById(R.id.run_button);
+        pause_button = (Button) findViewById(R.id.pause_button);
+        run_buton.setOnClickListener(this);
+        pause_button.setOnClickListener(this);
+
+        mConnectButton = (Button) findViewById(R.id.button_connect);
+        mConnectButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                findBT();
+                openBT();
+            }
+        });
+
+        mWaveform = (WaveformView) findViewById(R.id.WaveformArea);
+
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        int buttonID;
+        buttonID = v.getId();
+        switch (buttonID) {
+
+
+            case R.id.run_button:
+                    if (bready == true) {
+                        pause = false;
+                        sendMessage(prescaler_string);
+                        sendMessage(threshold_string);
+                        Toast.makeText(this, "Started....", Toast.LENGTH_SHORT).show();
+                        beginListenForData();
+                    } else {
+                        Toast.makeText(this, R.string.not_connected, Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+            case R.id.pause_button:
+                    pause = true;
+                    Toast.makeText(this, "Paused....", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
+    {
+        seekbar_value = progress;
+
+        switch (seekBar.getId())
+        {
+            case R.id.seekBar_prescaler:
+                int s = seekbar_value/10;
+                switch(s){
+                    case 0:
+                    case 1: prescaler_string = "p8";
+                        prescaler_value.setText ("008");
+                        break;
+                    case 3:
+                    case 2: prescaler_string = "p16";
+                        prescaler_value.setText ("016");
+                        break;
+                    case 4:
+                    case 5: prescaler_string = "p32";
+                        prescaler_value.setText ("032");
+                        break;
+                    case 6:
+                    case 7: prescaler_string = "p64";
+                        prescaler_value.setText ("064");
+                        break;
+                    default:
+                        prescaler_value.setText ("128");
+                        break;
+                        }
+
+                break;
+            case R.id.seekBar_threshold:
+               s = seekbar_value;
+               threshold_string = "t" + Integer.toString(s);
+                threshold_value.setText(Integer.toString(s));
+
+                    break;
+            case R.id.seekBar_frequency:
+                s = seekbar_value/10;
+                switch(s){
+                    case 0:
+                    case 1:
+                        mWaveform.setTimeBase(1);
+                        frequency_value.setText ("100 Hz");
+                        break;
+                    case 3:
+                    case 2:
+                        mWaveform.setTimeBase(2);
+                        frequency_value.setText ("1 kHz");
+                        break;
+                    case 4:
+                    case 5:
+                        mWaveform.setTimeBase(3);
+                        frequency_value.setText ("10 kHz");
+                        break;
+                    case 6:
+                    case 7:
+                        mWaveform.setTimeBase(4);
+                        frequency_value.setText ("100 kHz");
+                        break;
+                    case 8:
+                    case 9:
+                    case 10:
+                        mWaveform.setTimeBase(5);
+                        frequency_value.setText ("1 MHz");
+                        break;
+                    default:
+                        mWaveform.setTimeBase(1);
+                        frequency_value.setText ("100 Hz");
+                        break;
+
+                }
+                break;
+
+        }
+    }
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar)
+    {
+        // TODO Auto-generated method stub
+    }
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar)
+    {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+        // Stop the Bluetooth RFCOMM services
+        if (bready==true) closeBT();
+        // release screen being on
+        if (mWakeLock.isHeld()) {
+            mWakeLock.release();
+        }
+    }
+
+
+    void findBT()
+    {
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(mBluetoothAdapter == null)
+        {
+            Toast.makeText(this, "No adapter found", Toast.LENGTH_SHORT).show();
+        }
+
+        if(!mBluetoothAdapter.isEnabled())
+        {
+            Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBluetooth, 0);
+        }
+
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        if(pairedDevices.size() > 0)
+        {
+            for(BluetoothDevice device : pairedDevices)
+            {
+                if(device.getName().equals("HC-05"))
+                {
+                    mmDevice = device;
+                    break;
+                }
+            }
+        }
+        Toast.makeText(this, "Connected to HC-05 Bluetooth Module", Toast.LENGTH_SHORT).show();
+    }
+
+    void openBT()
+    {
+        UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+        try {//Standard SerialPortService ID
+            mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);
+        }catch (IOException e) { }
+
+        try {
+            mmSocket.connect();
+        }catch (IOException e) {}
+
+        try {
+            mmOutputStream = mmSocket.getOutputStream();
+            mmInputStream = mmSocket.getInputStream();
+            }catch (IOException e) { }
+
+        bready = true;
+    }
+
+    void beginListenForData()
+    {
+        final Handler handler = new Handler();
+        final byte data_start = 36;//This is ascii code for $
+         final byte data_end = 35;//This is the ASCII code #
+        stopWorker = false;
+        readBufferPosition = 0;
+        readBuffer = new int[1024];
+        workerThread = new Thread(new Runnable()
+        {
+            public void run()
+            {
+                while(!Thread.currentThread().isInterrupted() && !stopWorker)
+                {
+                    try
+                    {
+                        sendMessage(new String("s"));
+                        int bytesAvailable = mmInputStream.available();
+                        if(bytesAvailable > 0)
+                        {
+                            byte[] packetBytes = new byte[bytesAvailable];
+                            mmInputStream.read(packetBytes);
+                            for(int i=0;i<bytesAvailable;i++)
+                            {   int x = 0;
+                                byte b = packetBytes[i];
+                                if (b < 0) // if negative
+                                {
+                                    x = (int) ((b & 0x7F) + 128);
+                                }
+                                else
+                                {
+                                    x = (int) b;
+                                }
+                              /*  if (b == data_start) {
+                                    readBufferPosition = 0;
+                                }*/
+                                if(b == data_end)
+                                {
+                                    mWaveform.set_data(readBuffer);
+                                    readBufferPosition = 0;
+                                }
+                                else
+                                {
+                                    readBuffer[readBufferPosition++] = x;
+                                }
+                            }
+                        }
+                    }
+                    catch (IOException ex)
+                    {
+                        stopWorker = true;
+                    }
+                }
+            }
+        });
+        if(pause = false){
+            workerThread.start();
+        }
+
+        else {
+            sendMessage(new String("S"));
+        }
+    }
+
+    void sendMessage(String msg)
+    {
+        try {
+            mmOutputStream.write(msg.getBytes());
+        } catch (IOException e){}
+    }
+
+    void closeBT()
+    {
+        stopWorker = true;
+        try {
+            mmOutputStream.close();
+            mmInputStream.close();
+            mmSocket.close();
+        }catch(IOException e){}
+    }
+
+
+}
---
+++ app/src/main/java/oscilloscope/mijaz/com/bluescope/WaveformView.java
@@ -0,0 +1,138 @@
+package oscilloscope.mijaz.com.bluescope;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+import android.view.SurfaceHolder;
+
+public class WaveformView extends SurfaceView implements SurfaceHolder.Callback{
+
+    private WaveformPlotThread plot_thread;
+    private int time_setting = 1;
+
+    private final int width = 512;
+    private final int heigth = 255;
+
+    private static int[] ch1_data = new int[1024];
+    private static int ch1_pos = 127;
+
+    private Paint ch1_color = new Paint();
+    private Paint grid_paint = new Paint();
+    private Paint cross_paint = new Paint();
+    private Paint outline_paint = new Paint();
+
+    public WaveformView(Context context, AttributeSet attrs) {
+
+        super(context, attrs);
+        //super(context);
+        getHolder().addCallback(this);
+
+        int i;
+        for(i=0; i<width; i++){
+            ch1_data[i] = ch1_pos;
+        }
+
+        plot_thread = new WaveformPlotThread(getHolder(), this);
+        //setFocusable(true);
+        ch1_color.setColor(Color.YELLOW);
+        grid_paint.setColor(Color.rgb(100, 100, 100));
+        cross_paint.setColor(Color.rgb(70, 100, 70));
+        outline_paint.setColor(Color.GREEN);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width,
+                               int height) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        plot_thread.setRunning(true);
+        plot_thread.start();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        boolean retry = true;
+        plot_thread.setRunning(false);
+        while (retry){
+            try{
+                plot_thread.join();
+                retry = false;
+            }catch(InterruptedException e){
+
+            }
+        }
+
+    }
+
+    @Override
+    public void onDraw(Canvas canvas){
+        PlotPoints(canvas);
+
+    }
+
+    public void set_data(int[] data1 ){
+        int x;
+        plot_thread.setRunning(false);
+        x = 0;
+        while(x<width){
+            if(x<(data1.length)){
+                //ch1_data[x] = data1[x];
+                ch1_data[x] = heigth-data1[x]+1;
+            }else{
+                ch1_data[x] = ch1_pos;
+            }
+            x++;
+        }
+
+
+        plot_thread.setRunning(true);
+    }
+
+    public void setTimeBase(int val)
+    {
+        time_setting = val;
+    }
+
+    public void PlotPoints(Canvas canvas){
+
+        // clear screen
+        canvas.drawColor(Color.rgb(20, 20, 20));
+
+        // draw grids
+        for(int vertical = 1; vertical<10; vertical++){
+            canvas.drawLine(
+                    vertical*(width/10)+1, 1,
+                    vertical*(width/10)+1, heigth+1,
+                    grid_paint);
+        }
+        for(int horizontal = 1; horizontal<10; horizontal++){
+            canvas.drawLine(
+                    1, horizontal*(heigth/10)+1,
+                    width+1, horizontal*(heigth/10)+1,
+                    grid_paint);
+        }
+
+        // draw center cross
+        canvas.drawLine(0, (heigth/2)+1, width+1, (heigth/2)+1, cross_paint);
+        canvas.drawLine((width/2)+1, 0, (width/2)+1, heigth+1, cross_paint);
+
+        // draw outline
+        canvas.drawLine(0, 0, (width+1), 0, outline_paint);	// top
+        canvas.drawLine((width+1), 0, (width+1), (heigth+1), outline_paint); //right
+        canvas.drawLine(0, (heigth+1), (width+1), (heigth+1), outline_paint); // bottom
+        canvas.drawLine(0, 0, 0, (heigth+1), outline_paint); //left
+
+        // plot data
+        for(int x=0; x<(width-1); x+=time_setting){
+            canvas.drawLine(x+1, ch1_data[x], x+2, ch1_data[x+1], ch1_color);
+        }
+    }
+
+}
